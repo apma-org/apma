ab94e82c4450f5f12ebe6e99bfee61937df2d45a
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-04-15 15:46:09 -0400

    :wrench: configured tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,36 @@
+// tailwind.config.js
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      green: colors.green,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      transparent: "transparent",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+};
