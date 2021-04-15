5e9dac06afafc532de6efbb343da66c305b94aae
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-04-15 15:46:51 -0400

    added basic header file

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+      <header className="text-3xl font">
+        <div className="flex justify-center items-center bg-dark px-16 py-7 space-x-7 h-20">
+          APMA
+        </div>
+      </header>
+  );
+};
+
+
