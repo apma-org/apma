0d8b7c71d78c6839294ab42cb9fa9192cba6d895
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-04-18 14:45:50 -0400

    drafted template for pages

--- client/src/pages/Properties.jsx
+++ client/src/pages/Properties.jsx
@@ -3,21 +3,67 @@ import { PropertyCard } from "../components/PropertyCard";
 
 export const Properties = () => {
   const [totalProfits, setTotalProfits] = useState(8298.23);
-  const [properties, setProperties] = useState([]);
+  const [properties, setProperties] = useState([
+    {
+      maintenanceRequests: [
+        {
+          request: "Water heater is broken",
+        },
+        { request: "Sink is broken" },
+      ],
+      monthlyProfits: 4000,
+    },
+    {
+      maintenanceRequests: [
+        {
+          request: "The Lights are flickering",
+        },
+      ],
+      monthlyProfits: 3000,
+    },
+    {
+      maintenanceRequests: [
+        {
+          request: "The kitchen needs to be repainted",
+        },
+        { request: "The cabinet door is broken" },
+        { request: "The window needs washing" },
+      ],
+      monthlyProfits: 2500,
+    },
+    {
+      maintenanceRequests: [
+        {
+          request: "The garden needs to be planted",
+        },
+        { request: "There is no hot water." },
+      ],
+      monthlyProfits: 5450,
+    },
+    {
+      maintenanceRequests: [],
+      monthlyProfits: 7450,
+    },
+  ]);
 
   return (
-    <div>
-      Properties
-      <button className="bg-blue-200">Add New Property</button>
+    <div className="max-w-full text-black m-7 px-5 py-10 rounded-xl shadow-xl">
+      <h3 className="text-xl block justify-center text-center">Properties</h3>
+      <button className="bg-green-200 rounded-3xl p-3.5 hover:bg-green-300 text-white">
+        Add New Property
+      </button>
       <h3>Total Profits: ${totalProfits} / month</h3>
-      <div className="bg-pink-100">
-        {
-          properties.map((e) => {
-            <PropertyCard/>
-          })
-        }
+      <div className="bg-pink-100 text-black grid justify-items-stretch">
+        ljhbl
+        {console.log("properties", properties)}
+        {properties != null &&
+          properties.map((e) => (
+            <PropertyCard
+              maintenanceRequests={e.maintenanceRequests}
+              monthlyProfits={e.monthlyProfits}
+            />
+          ))}
       </div>
     </div>
   );
 };
-
--- client/src/pages/Property.jsx
+++ client/src/pages/Property.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
-import { Expenses } from '../components/Expenses'
+import { Expenses } from "../components/Expenses";
 import { UnitCard } from "../components/UnitCard";
 
 export const Property = () => {
   const [totalProfits, setTotalProfits] = useState(8298.23);
-  const [units, setUnits] = useState({});
+  const [units, setUnits] = useState([
+    { profits: 5000 },
+    { profits: 2000 },
+    { profits: 1000 },
+    { profits: 5500 },
+  ]);
 
   return (
     <div className="bg-red-300">
       Property 123 Bob Avenue
-
       <button className="bg-blue-200">Edit Property</button>
       <h3>Total Profits: ${totalProfits} / month</h3>
-      <Expenses/>
+      <Expenses />
       <div>
-        {
-        units.map((e, idx) => {
-          <UnitCard key={idx} occupied={e.occupied} tenantName={e.tenantName} rent= {e.rent} maintenanceRequests={e.maintenanceRequests} unpaid = {e.unpaid} />
-        })
-        }
+        {units &&
+          units.map((e, idx) => (
+            <UnitCard
+              key={idx}
+              occupied={e.occupied}
+              tenantName={e.tenantName}
+              rent={e.rent}
+              maintenanceRequests={e.maintenanceRequests}
+              unpaid={e.unpaid}
+            />
+          ))}
       </div>
     </div>
   );
