d59e217c927b4706d7ad5a35a20d3da844c8bc42
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-04-19 20:32:26 -0400

    :hammer: simple form validation + handling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,29 +1,61 @@
-import React from "react";
-import user from "../images/user.svg";
-import logo from "../images/logo.svg";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export const Login = () => {
+  const history = useHistory();
+  const [loginInfo, setLoginInfo] = useState({});
+
+  const handleChange = (e) => {
+    setLoginInfo({
+      ...loginInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("login", loginInfo);
+    // const { user, error } = await loginUser(loginInfo);
+    // if (user) {
+    history.push("/properties");
+    // } else {
+    // console.log("ERRRR");
+    // }
+  };
+
   return (
     <div className="max-w-lg text-white mx-auto bg-green-100 px-5 py-10 rounded-xl shadow-xl">
       <div className="text-center mb-8">
         <h1 className="text-2xl">LOGIN</h1>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mt-5">
           <label>Email</label>
-          <input type="text" class="block w-full p-2 border-none rounded-lg" />
+          <input
+            required
+            type="email"
+            name="email"
+            className="text-gray-900 block w-full p-2 border-none rounded-lg"
+            onChange={handleChange}
+          />
         </div>
         <div className="mt-5">
           <label>Password</label>
-          <input type="text" class="block w-full p-2 border-none rounded-lg" />
-        </div>
-        <div className="mt-10">
           <input
-            type="submit"
-            class="py-3 bg-green-200 text-white w-full hover:bg-green-300 rounded-xl"
+            required
+            type="password"
+            name="password"
+            className="text-gray-900 block w-full p-2 border-none rounded-lg"
+            onChange={handleChange}
           />
         </div>
+        <button
+          type="submit"
+          className="mt-10 py-3 bg-green-200 text-white w-full hover:bg-green-300 rounded-xl"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
