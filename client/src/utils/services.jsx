dea08335cae237c738e961f749dfb0d1370d2072
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-04-21 16:18:39 -0400

    added all services

--- client/src/utils/services.jsx
+++ client/src/utils/services.jsx
@@ -1,9 +1,8 @@
 import axios from "axios";
-
 const baseUrl = "https://apma-backend.herokuapp.com/";
 
 /**
- * Register User
+ * Register User [Tenant or Landowner]
  * @param {*} registerInfo first_name, last_name, email, password
  * @param {*} userType Tenant or Landowner
  */
@@ -46,8 +45,59 @@ export const getLandowner = async (landownerId) => {
   if (user.status === 200) {
     return user;
   } else {
-    console.log("Something went wrong");
+    console.log("Something went wrong getting this landowner");
     return;
+  }
+};
+
+/**
+ * Update Landowner Info
+ * @param {*} landownerId Landowner Id number
+ * @param {*} landownerInfo first_name, last_name, email, password
+ * @returns Modified Landowner Object
+ */
+export const editLandowner = async (landownerId, landownerInfo) => {
+  const user = await axios.put(`${baseUrl}landowner/${landownerId}`, {
+    ...landownerInfo,
+  });
+
+  if (user.status === 400) {
+    console.log("Something went wrong updating this landowner");
+    return;
+  } else {
+    return user;
+  }
+};
+
+/**
+ * Get Tenant Info
+ * @param {*} tenantId Landowner Id number
+ * @returns Landowner Object
+ */
+export const getTenant = async (tenantId) => {
+  const user = await axios.get(`${baseUrl}tenant/${tenantId}`);
+
+  if (user.status === 400) {
+    console.log("Something went wrong getting this landowner");
+    return;
+  } else {
+    return user;
+  }
+};
+
+/**
+ * Update Tenant Info
+ * @param {*} tenantId Tenant Id number
+ * @param {*} tenantInfo first_name, last_name, email, password, (optional) unit_ids
+ * @returns Modified Tenant Object
+ */
+export const editTenant = async (tenantId, tenantInfo) => {
+  const user = await axios.put(`${baseUrl}tenant/${tenantId}`, {
+    ...tenantInfo,
+  });
+
+  if (user.status === 200) {
+    return user.message;
   }
 };
 
@@ -59,7 +109,7 @@ export const getLandowner = async (landownerId) => {
  */
 export const getProperty = async (propertyId) => {
   const property = await axios.get(`${baseUrl}property/${propertyId}`);
-
+  console.log("get property", property);
   if (property.status === 400) {
     console.log("Something went wrong with getting this property");
     return;
@@ -71,7 +121,7 @@ export const getProperty = async (propertyId) => {
 /**
  * Add Property
  * @param {*} propertyInfo 	landowner_id, zipcode, city, state, address, mortgage, tax, insurance, appreciation
- * @returns Proper
+ * @returns Property Object
  */
 export const addProperty = async (propertyInfo) => {
   const property = await axios.post(`${baseUrl}property`, { ...propertyInfo });
@@ -89,7 +139,7 @@ export const addProperty = async (propertyInfo) => {
  * Update Property Info
  * @param {*} propertyInfo landowner_id, zipcode, city, state, address, mortgage, tax, insurance, appreciation
  * @param {*} propertyId Property Id number
- * @returns Updated Property Object
+ * @returns Modified Property Object
  */
 export const editProperty = async (propertyInfo, propertyId) => {
   const property = await axios.put(`${baseUrl}property/${propertyId}`, {
@@ -105,6 +155,14 @@ export const editProperty = async (propertyInfo, propertyId) => {
 };
 
 /**
+ * Delete Property
+ * @param {*} propertyId Property Id number
+ */
+export const deleteProperty = async (propertyId) => {
+  await axios.delete(`${baseUrl}property/${propertyId}`);
+};
+
+/**
  * Get Unit Info
  * @param {*} unitId Unit Id number
  * @returns Unit Object
@@ -112,35 +170,128 @@ export const editProperty = async (propertyInfo, propertyId) => {
 export const getUnit = async (unitId) => {
   const unit = await axios.get(`${baseUrl}unit/${unitId}`);
 
-  if (!unit.id) {
-    console.log("Something went wrong");
+  if (unit.status === 400) {
+    console.log("Something went wrong getting this unit");
     return;
   } else {
     return unit;
   }
 };
 
+/**
+ * Add Unit to Property
+ * @param {*} unitInfo lease, rent_amount, rent_deposit, property_id
+ * @returns Success Message
+ */
 export const addUnit = async (unitInfo) => {
   const unit = await axios.post(`${baseUrl}unit`, { ...unitInfo });
 
-  if (!unit.property_id) {
-    console.log("Something went wrong");
-    return;
+  if (unit.status === 200) {
+    return unit.message;
   } else {
-    return unit;
+    console.log("Something went wrong with adding this unit");
+    return;
   }
 };
 
+/**
+ * Update Unit
+ * @param {*} unitInfo (all fields optional) lease, rent_amount, rent_deposit, property_id (optional)
+ * @param {*} unitId Unit Id number
+ * @param {*} propertyId Property Id number in which the unit belongs to
+ * @returns Modified Unit Object
+ */
 export const editUnit = async (unitInfo, unitId, propertyId) => {
-  const property = await axios.put(`${baseUrl}unit/${unitId}`, {
+  const unit = await axios.put(`${baseUrl}unit/${unitId}`, {
     ...unitInfo,
     property_id: propertyId,
   });
 
-  if (!property.id) {
-    console.log("Something went wrong");
+  if (unit.status === 400) {
+    console.log("Something went wrong with updating this unit");
     return;
   } else {
-    return property;
+    return unit;
   }
+};
+
+/**
+ * Delete Unit
+ * @param {*} unitId Unit Id number
+ * @returns Success Status
+ */
+export const deleteUnit = async (unitId) => {
+  await axios.delete(`${baseUrl}unit/${unitId}`);
+};
+
+// TODO: Is Maintenance request field a decimal?
+/**
+ * Add Maintenance Request to Unit
+ * @param {*} maintenanceInfo unit_id, request
+ * @returns Success Message
+ */
+export const addMaintenance = async (maintenanceInfo) => {
+  const maintenance = await axios.post(`${baseUrl}maintenannce`, {
+    ...maintenanceInfo,
+  });
+
+  if (maintenance.status === 200) {
+    return maintenance.message;
+  } else {
+    console.log("Something went wrong with adding this request");
+    return;
+  }
+};
+
+/**
+ * Get Maintenance Request
+ * @param {*} maintenanceId Maintenance Id number
+ * @returns Maintenance Object
+ */
+export const getMaintenance = async (maintenanceId) => {
+  const maintenance = await axios.get(`${baseUrl}maintenance/${maintenanceId}`);
+
+  if (maintenance.status === 400) {
+    console.log("Something went wrong with getting this request");
+    return;
+  } else {
+    return maintenance;
+  }
+};
+
+/**
+ * Update Maintenance
+ * @param {*} maintenanceInfo unit_id, request, date_fixed
+ * @param {*} maintenanceId Maintenance Id number
+ * @param {*} unitId Unit Id number that maintenance request belongs to
+ * @returns Modified Maintenance Object
+ */
+export const editMaintenance = async (
+  maintenanceInfo,
+  maintenanceId,
+  unitId
+) => {
+  const maintenance = await axios.put(
+    `${baseUrl}maintenance/${maintenanceId}`,
+    {
+      ...maintenanceInfo,
+      unit_id: unitId,
+    }
+  );
+
+  if (maintenance.status === 400) {
+    console.log("Something went wrong with updating this request");
+    return;
+  } else {
+    return maintenance;
+  }
+};
+
+/**
+ * Delete Maintenance
+ * @param {*} maintenanceId Maintenance Id number
+ * @returns Success Status
+ */
+export const deleteMaintenance = async (maintenanceId) => {
+  await axios.delete(`${baseUrl}maintenance/${maintenanceId}`);
 };
