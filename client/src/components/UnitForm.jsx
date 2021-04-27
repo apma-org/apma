b8aa063e584ee2133490efafca0669723ba86d11
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-04-27 17:50:51 -0400

    removed add property/unit route and workflow

--- client/src/components/UnitForm.jsx
+++ client/src/components/UnitForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { editUnit } from "../utils/services";
+import { addUnit, editUnit } from "../utils/services";
 
 export const UnitForm = ({
   unit_id,
@@ -7,25 +7,34 @@ export const UnitForm = ({
   rent_amount,
   rent_deposit,
   lease,
-  close
+  close,
+  isAdding,
 }) => {
-  const [unitInfo, setUnitInfo] = useState({
-    rent_amount,
-    rent_deposit,
-    lease,
-  });
+  const [unitInfo, setUnitInfo] = useState(
+    unit_id
+      ? {
+          rent_amount,
+          rent_deposit,
+          lease,
+        }
+      : {
+          rent_amount: null,
+          rent_deposit: null,
+          lease: "",
+        }
+  );
 
   const handleChange = ({ target: { name, value } }) => {
     setUnitInfo((prevState) => ({ ...prevState, [name]: value }));
-    // if (name === "rent_deposit") value = value.replace(/^\d+\.\d{0,2}$/, "");
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("FORUM", { ...unitInfo, property_id: property_id });
     if (unitInfo) {
-      const data = await editUnit(unitInfo, unit_id, property_id);
-      close(data)
+      const data = isAdding
+        ? await addUnit({ ...unitInfo, property_id: property_id })
+        : await editUnit(unitInfo, unit_id, property_id);
+      close(data);
     }
   };
 
@@ -38,12 +47,11 @@ export const UnitForm = ({
           type="number"
           step="0.01"
           name="rent_amount"
-          defaultValue={rent_amount || 0.0}
+          defaultValue={rent_amount || null}
           className="text-gray-900 block w-full p-2 border-none rounded-lg"
           onChange={handleChange}
         />
       </div>
-      {/* TODO: ADD SEARCH-BAR FOR SELECTING TENANT BY TENANT_ID */}
       <div className="mt-5">
         <label>Rent Deposit</label>
         <input
@@ -51,7 +59,7 @@ export const UnitForm = ({
           type="text"
           step="0.01"
           name="rent_deposit"
-          defaultValue={rent_deposit || 0.0}
+          defaultValue={rent_deposit || null}
           className="text-gray-900 block w-full p-2 border-none rounded-lg"
           onChange={handleChange}
         />
